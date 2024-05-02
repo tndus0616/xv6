a2f70defb27cf0c450ed92acbe49edd2e2a29e8b
manjudeli <151948222+manjudeli@users.noreply.github.com>
2024-05-02 20:21:28 +0900

    Update trap.c

--- xv6/trap.c
+++ xv6/trap.c
@@ -56,15 +56,12 @@ trap(struct trapframe *tf)
       release(&tickslock);
     }
     lapiceoi();
-    // if(myproc() && myproc()->state == RUNNING){
-    //     void (*scheduler)(void) = (void (*)(void))myproc()->scheduler;
-    //     if(scheduler != 0) {
-    //         scheduler();
-    //     }
-    // }
-    if (myproc() && myproc()->state == RUNNING) {
-    // 스케줄러 호출이 보장되도록 조건을 단순화
-    thread_schedule();
+    if(myproc() && myproc()->state == RUNNING){
+        void (*scheduler)(void) = (void (*)(void))myproc()->scheduler;
+        if(scheduler != 0) {
+            scheduler();
+        }
+    }
     }
     break;
   case T_IRQ0 + IRQ_IDE:
